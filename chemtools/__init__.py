6032d71c1b85f7889b36a1d2ee1cb2c532c779f7
Matteo Griot <matteo.griot@gmail.com>
2024-08-21 20:15:41 +0200

    Update __init__.py

---
+++ chemtools/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Author: MGriot
+# Date: 21/08/2024
+#
+# This code is protected by copyright and cannot be 
+# copied, modified, or used without the explicit 
+# permission of the author. All rights reserved.
+# ---------------------------------------------------------
