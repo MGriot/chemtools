9afd5ae6b56b6cfba7e031a5a710c9a81e416575
Matteo <matteo.griot@gmail.com>
2024-08-25 13:34:29 +0200

    update before mass class update

---
+++ PCA.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from chemtools.exploration import PrincipalComponentAnalysis
+
+# Creazione di un database con nomi degli oggetti e titoli delle variabili
+data = {
+    "Nome Oggetto": ["Oggetto1", "Oggetto2", "Oggetto3", "Oggetto4"],
+    "Variabile1": [1.2, 2.3, 3.1, 4.5],
+    "Variabile2": [3.4, 1.2, 4.5, 2.3],
+    "Variabile3": [5.6, 3.4, 2.1, 1.2],
+    "Variabile4": [7.8, 6.5, 3.2, 4.1],
+    "Variabile5": [2.3, 4.6, 1.8, 3.9],
+    "Variabile6": [9.0, 8.1, 7.2, 6.3],
+}
+
+# Creazione del DataFrame
+df = pd.DataFrame(data)
+
+# Visualizzazione del DataFrame
+print("Database creato:")
+print(df)
+
+# Preparazione dei dati per la PCA (escludendo la colonna 'Nome Oggetto')
+X = df.drop("Nome Oggetto", axis=1)
+
+pca = PrincipalComponentAnalysis(X)
+pca.fit()
+# pca.plot_correlation_matrix()
+pca.plot_eigenvalue()
